7587c9dd057b8d380ff8bb7b59761ca7241b2a9d
naveenkarri383 <karrinaveen.mme@gmail.com>
2024-02-24 02:01:52 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # US-visa-approval-prediction
 
+## Git commads for committing the code
+""" git add .
+    git commit -m "comitted"
+    git push origin main
+
+"""
+
+## Run the code
+"""
+    create python environment 
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="us_visa_application",
+    version="0.0.0",
+    author="Naveen",
+    author_email="karrinaveen.mme@gmail.com",
+    packages=find_packages()  # look for constructor and find that as a local package
+)
